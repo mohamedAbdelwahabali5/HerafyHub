a5970b29189389f52f1d0162397d901d71896d36
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-02-17 22:25:20 +0200

    adding login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
