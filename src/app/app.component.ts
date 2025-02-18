506b33b3df9c7447dd178dfeac82c7c6b10c632b
Omar Araby <omararaby789@gmail.com>
2025-02-18 22:08:28 +0200

    handle product details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CollectionComponent } from './CollectionComponent/collection/collection.component';
 import { LoginComponent } from './Components/login/login.component';
 import { HeroComponent } from './Components/hero/hero.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeroComponent, CollectionComponent],
+  imports: [ProductDetailsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
