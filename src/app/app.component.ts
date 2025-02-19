8de38b44bee0ac61bf07da756bb27d6ce2786164
sama ibrahim <samaibrahim000074@gmail.com>
2025-02-19 13:20:12 +0200

    footer and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [LoginComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
