d9a3307dff84ce4bac2977dd98be7c28049e990e
OmarAraby <omararaby789@gmail.com>
2025-02-23 22:43:43 +0200

    initial RouterOutlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
+import { LoginComponent } from './Components/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', redirectTo:'home' ,pathMatch: 'full'},
+{path: 'home', component:HomeComponent},
+{path: 'login', component:LoginComponent},
+{path: 'about-us', component:AboutUsComponent},
+{path: 'product-details/:id', component:ProductDetailsComponent} ,
+// {path:'**',component:ErrorComponent},
+
+];
+
