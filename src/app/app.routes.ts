3ca6f30f332ee4f08b49ebb253b1544ec2a788e2
OmarAraby <omararaby789@gmail.com>
2025-02-25 14:46:56 +0200

    initial error comp..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { HomeComponent } from './Components/home/home.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 import { LoginComponent } from './Components/login/login.component';
+import { ErrorComponent } from './Components/error/error.component';
 
 
 export const routes: Routes = [
-    {path: '', redirectTo:'home' ,pathMatch: 'full'},
-{path: 'home', component:HomeComponent},
-{path: 'login', component:LoginComponent},
-{path: 'about-us', component:AboutUsComponent},
-{path: 'product-details/:id', component:ProductDetailsComponent} ,
-// {path:'**',component:ErrorComponent},
+        {path: '', redirectTo:'home' ,pathMatch: 'full'},
+        {path: 'home', component:HomeComponent},
+        {path: 'login', component:LoginComponent},
+        {path: 'about-us', component:AboutUsComponent},
+        {path: 'product-details/:id', component:ProductDetailsComponent} ,
+        {path:'**',component:ErrorComponent},
+        
 
 ];
 
