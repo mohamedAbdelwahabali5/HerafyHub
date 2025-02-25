bb499888ddbafe347aadd985c9cd0945ad0a7d96
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-02-25 16:50:25 +0200

    Added new images and a helper service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,22 @@ import { HeroComponent } from './Components/hero/hero.component';
 import { CategoryInfoComponent } from './Components/category-info/category-info.component';
 import { ProductsListComponent } from './Components/products-list/products-list.component';
 import { ProductCardComponent } from './Components/product-card/product-card.component';
-
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { RegistrationComponent } from "./Components/registration/registration.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent,
-    FooterComponent,
-    ProductsListComponent,
-    CategoryInfoComponent,
-
-  ],
+    // HeaderComponent,
+    // FooterComponent,
+    // ProductsListComponent,
+    // CategoryInfoComponent,
+    // LoginComponent,
+    // ForgotPasswordComponent,
+    ResetPasswordComponent,
+    // RegistrationComponent
+],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
