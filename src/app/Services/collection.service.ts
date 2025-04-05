8fc7ae44e86e346696558825fffc2f99719e3889
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-04-06 00:02:10 +0200

    handle slider successfully

--- src/app/Services/collection.service.ts
+++ src/app/Services/collection.service.ts
@@ -14,11 +14,13 @@ export class ProductService {
   private readonly products_URL = `${this.ApiUrl}/product`;
   private readonly all_Products_URL = `${this.ApiUrl}/product/all`;
   private readonly searchProduct_URL = `${this.ApiUrl}/search`;
-  private readonly Categories_URL = `${this.ApiUrl}/category`;  
+  private readonly Categories_URL = `${this.ApiUrl}/category`;
+
   constructor(
     private http: HttpClient,
     private usersService: UsersService
-  ) {}
+  ) { }
+
   private getHeaders() {
     const token = this.usersService.getToken();
     return new HttpHeaders({
@@ -26,6 +28,7 @@ export class ProductService {
       'Content-Type': 'application/json'
     });
   }
+
   getAllProducts() {
     return this.http.get(this.all_Products_URL, { headers: this.getHeaders() });
   }
@@ -33,30 +36,31 @@ export class ProductService {
   getProductsCategory(page: number, pageSize: number, categoryId: string = '') {
     let url = `${this.products_URL}/?page=${page}&limit=${pageSize}`;
     if (categoryId) {
-      url += `& categoryId=${categoryId}`;
+      url += `&categoryId=${categoryId}`;
     }
     return this.http.get(url, { headers: this.getHeaders() });
   }
+
   searchByTitleInCategory(title: string, categoryId: string) {
     console.log('Searching for:', title, 'in category:', categoryId);
     if (categoryId == 'allProducts') {
-      return this.http.get(`${this.all_Products_URL}, { headers: this.getHeaders() }`);
+      return this.http.get(`${this.all_Products_URL}`, { headers: this.getHeaders() });
     } else {
-      return this.http.get(`${this.searchProduct_URL} ? title = ${title} & categoryId=${categoryId}`,
+      return this.http.get(`${this.searchProduct_URL}?title=${title}&categoryId=${categoryId}`,
         { headers: this.getHeaders() }
       );
     }
   }
+
   getAllCategories() {
     return this.http.get(this.Categories_URL, { headers: this.getHeaders() });
   }
 
   getCategoryById(categoryId: string) {
-    return this.http.get(`${this.Categories_URL} / ${categoryId}, { headers: this.getHeaders() }`);
+    return this.http.get(`${this.Categories_URL}/${categoryId}`, { headers: this.getHeaders() });
   }
 
   getProductById(id: string) {
-    return this.http.get(`${this.products_URL} / ${id}, { headers: this.getHeaders() }`);
+    return this.http.get(`${this.products_URL}/${id}`, { headers: this.getHeaders() });
   }
 }
-
