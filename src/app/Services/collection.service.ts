0de71b7d427e00e140d7506b42d700903f94ff47
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-04-05 08:21:08 +0200

    Handling APIs access Services

--- src/app/Services/collection.service.ts
+++ src/app/Services/collection.service.ts
@@ -2,25 +2,20 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap, catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
-import {Product,ProductApiResponse} from '../Utils/interface'
-
+import { Product, ProductApiResponse } from '../Utils/interface'
+import { environment } from '../../environments/environment.prod';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  // private readonly Collection_URL = 'https://fakestoreapi.com/products';
-  // private readonly products_URL = 'http://localhost:5555/product';
-  // private readonly all_Products_URL = 'http://localhost:5555/product/all';
-  // private readonly searchProduct_URL = 'http://localhost:5555/product/search';
-  // private readonly Categories_URL = 'http://localhost:5555/category';
+  private ApiUrl = environment.apiUrl;
+  private readonly products_URL = `${this.ApiUrl}/product`;
+  private readonly all_Products_URL = `${this.ApiUrl}/product/all`;
+  private readonly searchProduct_URL = `${this.ApiUrl}/search`;
+  private readonly Categories_URL = `${this.ApiUrl}/category`;
 
-  private readonly products_URL = 'https://herafy-hub-api.vercel.app/product';
-  private readonly all_Products_URL = 'https://herafy-hub-api.vercel.app/product/all';
-  private readonly searchProduct_URL = 'https://herafy-hub-api.vercel.app/search';
-  private readonly Categories_URL = 'https://herafy-hub-api.vercel.app/category';
-  
-  constructor(private http: HttpClient) {}
-  
+  constructor(private http: HttpClient) { }
+
   //Handle All collection
   getAllProducts() {
     return this.http.get(this.all_Products_URL);
@@ -32,27 +27,27 @@ export class ProductService {
     }
     return this.http.get(url);
   }
-  
+
   //Handle search
   searchByTitleInCategory(title: string, categoryId: string) {
     console.log('Searching for:', title, 'in category:', categoryId);
     if (categoryId == 'allProducts') {
       return this.http.get(`${this.all_Products_URL}`);
-    }else {
+    } else {
       return this.http.get(`${this.searchProduct_URL}?title=${title}&categoryId=${categoryId}`);
     }
   }
-  
+
   //Handle all Categories
   getAllCategories() {
     return this.http.get(this.Categories_URL);
   }
-  
+
   // Get category by ID
   getCategoryById(categoryId: string) {
     return this.http.get(`${this.Categories_URL}/${categoryId}`);
   }
-  
+
   // get product by id --> single product
   getProductById(id: string) {
     return this.http.get(`${this.products_URL}/${id}`);
