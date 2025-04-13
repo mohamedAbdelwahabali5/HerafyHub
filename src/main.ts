25cb8b57a3c9265c9de89be95109f00bc4890c0b
OmarAraby <omararaby789@gmail.com>
2025-04-13 10:37:33 +0200

    edit style in style.css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { mergeApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, appConfig)
+const config = mergeApplicationConfig(appConfig, {
+  providers: [
+    provideClientHydration()
+  ]
+});
+
+bootstrapApplication(AppComponent, config)
   .catch((err) => console.error(err));
