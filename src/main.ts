95a5e964ec00d19e863f93867f83c8a84a01fadf
OmarAraby <omararaby789@gmail.com>
2025-04-15 19:21:59 +0200

    Handle profile img logic

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { mergeApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
 
-const config = mergeApplicationConfig(appConfig, {
-  providers: [
-    provideClientHydration()
-  ]
-});
-
-bootstrapApplication(AppComponent, config)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
